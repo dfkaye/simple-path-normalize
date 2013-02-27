e09a358d07702ba44b24bb83872717dec880b754
david kaye <dfkaye@yahoo.com>
2013-02-27 10:03:12 -0800

    more tests

--- test/path-normalize-test.js
+++ test/path-normalize-test.js
@@ -5,3 +5,28 @@ test('empty should return /', function (t) {
   t.plan(1);
   t.equal(normalize(''), '/');
 });
+
+test('slash should return /', function (t) {
+  t.plan(1);
+  t.equal(normalize('/'), '/');
+});
+
+test('double slash should return /', function (t) {
+  t.plan(1);
+  t.equal(normalize('//'), '/');
+});
+
+test('dot should return /', function (t) {
+  t.plan(1);
+  t.equal(normalize('.'), '/');
+});
+
+test('double dots should return /', function (t) {
+  t.plan(1);
+  t.equal(normalize('..'), '/');
+});
+
+test('$1 should return $2', function (t) {
+  t.plan(1);
+  t.fail('next test to implement');
+});
