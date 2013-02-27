cefecc9f16a399ffc398aa0acc9c6a890b28976b
david kaye <dfkaye@yahoo.com>
2013-02-27 09:50:48 -0800

    update test

---
+++ test/path-normalize-test.js
@@ -0,0 +1,7 @@
+﻿var test = require('tape');
+var normalize = require('../path-normalize.js').normalize;
+
+test('empty should return /', function (t) {
+  t.plan(1);
+  t.equal(normalize(''), '/');
+});
